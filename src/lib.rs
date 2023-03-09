1c49b4b292acd43ac116dd3b482414d4b0d338e3
s.denisov <go.prog.freelance@gmail.com>
2023-03-09 15:59:25 +0300

    add all

---
+++ src/lib.rs
@@ -0,0 +1,17 @@
+use wasm_bindgen::prelude::*;
+
+#[wasm_bindgen]
+extern {
+    pub fn alert(s: &str);
+}
+
+#[wasm_bindgen]
+pub fn greet(name: &str) {
+    alert(&format!("Hello, {}!", name));
+}
+
+#[wasm_bindgen]
+pub fn tester(name: &str) -> String {
+    return "Hello".to_owned() + name
+}
+//wasm-pack build --target web
